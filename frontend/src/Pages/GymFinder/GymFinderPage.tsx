87353900e0b5a05cd6d0425168be586d5439b10b
Eric Zhou <zdog74721@gmail.com>
2024-03-19 11:47:16 -0400

    More Structuring

---
+++ frontend/src/Pages/GymFinder/GymFinderPage.tsx
@@ -0,0 +1,8 @@
+const GymFinderPage = () => {
+    return (
+    <div>
+    <h1>Herro Test</h1>
+    </div>);
+}
+
+export default GymFinderPage;
---
+++ frontend/src/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import {
+    createBrowserRouter,
+    RouterProvider
+} from "react-router-dom";
+
+import ErrorPage from "./Pages/ErrorPage/ErrorPage"
+import GymFinderPage from "./Pages/GymFinder/GymFinderPage"
+import HomePage from "./Pages/HomePage/HomePage"
+import MealPage from "./Pages/Meals/MealPage"
+import SupplementPage from "./Pages/Supplements/SupplementPage"
+import WorkoutPage from "./Pages/Workouts/WorkoutPage"
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <HomePage/>,
+        errorElement: <ErrorPage/>,
+    },
+    {
+        path: '/gymfinder',
+        element: <GymFinderPage/>
+    },
+    {
+        path: '/workouts',
+        element: <WorkoutPage/>
+    },
+    {
+        path:'/mealplans',
+        element: <MealPage/>
+    },
+    {
+        path:'/supplements',
+        element: <SupplementPage/>
+    }
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
