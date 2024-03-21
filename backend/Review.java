1a6e938c70b8bd100433ce3bcb8dc8e6ba837bc7
Kaley Du <kaley.du1@gmail.com>
2024-03-21 16:39:10 -0400

    actually actually fixed review:

--- backend/Review.java
+++ backend/Review.java
@@ -1,19 +1,19 @@
 public class Review {
 
-	private String reviewerID;
+	private String reviewID;
 	private String reviewerName;
 	private int	reviewerRating;
 	private String reviewerDesc;
 
 	public Review(String reviewID, String reviewerName, int reviewerRating, String reviewerDesc){
-		this.reviewerID = reviewID;
+		this.reviewID = reviewID;
 		this.reviewerName = reviewerName;
 		this.reviewerRating = reviewerRating;
 		this.reviewerDesc = reviewerDesc;
 	}
 
 	public String getReviewerID() {
-		return reviewerID;
+		return reviewID;
 	}
 
 	public void setReviewerID(String reviewerID) {
