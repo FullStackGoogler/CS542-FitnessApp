89735d7dd4f584037f725b0c9d76d70a59f1db0f
Eric Zhou <zdog74721@gmail.com>
2024-03-28 16:21:42 -0400

    Database API Connection + Package Lock Shenanigans

--- backend/database.js
+++ backend/database.js
@@ -1,19 +1,27 @@
-var pg = require('pg');
+const express = require('express');
+const { Pool } = require('pg');
 
-var client = new pg.Client({
+const app = express();
+
+var pool = new Pool({
     user: "postgres",
     password: "DataFitness8!",
     database: "postgres",
     port: 5432,
     host: "finalcs542.cle4qgwiih1x.us-east-2.rds.amazonaws.com",
-    ssl: true
+    ssl: { rejectUnauthorized: false },
 });
 
-try{
-  client.connect();
-  console.log("successfully connected");
-}
-catch(error){
-    console.error('Error connecting:', error);
-    throw error;
-}
+app.get('/api/exercises', async (req, res) => {
+    try {
+      const result = await pool.query('SELECT * FROM Exercise');
+      res.json(result.rows);
+    } catch (err) {
+      console.error('Error executing query:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+app.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
