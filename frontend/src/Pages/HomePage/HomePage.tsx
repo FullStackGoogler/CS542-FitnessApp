2dbde0be9dbc1e1c2612810c3cd7c2555bc01de4
Eric Zhou <ezzhou7@gmail.com>
2024-04-13 20:23:39 -0400

    Added Favorite Supplements/Nutrition Plan to HomePage

--- frontend/src/Pages/HomePage/HomePage.tsx
+++ frontend/src/Pages/HomePage/HomePage.tsx
@@ -3,10 +3,17 @@ import TopBar from "../../Components/TopBar";
 import { FormControl, InputLabel, Select, MenuItem, Typography, Divider } from "@mui/material";
 
 import { WorkoutItem } from "../Workouts/Interfaces/WorkoutItem";
+import { NutritionPlanItem } from "../NutritionPlans/Interfaces/NutritionPlanItem";
+import { SupplementItem } from "../Supplements/Interfaces/SupplementItem";
 
 const HomePage = () => {
     const [userPrograms, setUserPrograms] = useState<WorkoutItem[]>([]);
     const [selectedProgram, setSelectedProgram] = useState<WorkoutItem | null>(null);
+
+    const [supplements, setSupplements] = useState<SupplementItem[]>([]);
+
+    const [nutritionPlans, setNutritionPlans] = useState<NutritionPlanItem[]>([]);
+
     const [currentDay, setCurrentDay] = useState<number>(0);
     const currentDate = new Date();
 
@@ -17,8 +24,9 @@ const HomePage = () => {
         day: 'numeric'
     });
 
-    //Fetch all User Programs on page load that the current user has favorited
+    //Fetch all favorited items by the current user
     useEffect(() => {
+        //Fetch all User Programs on page load that the current user has favorited
         fetch('http://localhost:9000/api/userprograms')
             .then(response => response.json())
             .then(data => {
@@ -46,6 +54,65 @@ const HomePage = () => {
             })
             .catch(error => console.error('Error fetching user programs:', error));
     
+        fetch('http://localhost:9000/api/supplements')
+            .then(response => response.json())
+            .then(data =>  {
+                fetch('http://localhost:9000/api/takes/1')
+                    .then(response => response.json())
+                    .then(userUses => {
+                        const supplementIDsUsed = userUses.map((supplement: any) => supplement.supplementid);
+
+                        const filteredSupplements = data
+                            .filter((supplement: any) => supplementIDsUsed.includes(supplement.supplementid))
+                            .map((supplement: any) => ({
+                                supplementid: supplement.supplementid,
+                                product_name: supplement.product_name,
+                                product_category: supplement.product_category,
+                                product_description: supplement.product_description,
+                                brand_name: supplement.brand_name,
+                                link:supplement.link,
+                                price:supplement.price,
+                                price_per_serving:supplement.price_per_serving,
+                                overall_rating:supplement.overall_rating,
+                                number_of_reviews: supplement.number_of_reviews,
+                                verified_buyer_rating: supplement.verified_buyer_rating,
+                                verified_buyer_number: supplement.verified_buyer_number,
+                                top_flavor_rated: supplement.top_flavor_rated,
+                                number_of_flavors: supplement.number_of_flavors,
+                                average_flavor_rating: supplement.average_flavor_rating
+                            }));
+
+                        setSupplements(filteredSupplements);
+                    })
+                    .catch(error => console.error('Error fetching user-followed supplements:', error));
+            })
+            .catch(error => console.error('Error fetching supplements:', error));
+        
+        fetch('http://localhost:9000/api/nutritionplan')
+            .then(response => response.json())
+            .then(data =>  {
+                fetch('http://localhost:9000/api/userfollowsnutritionplan/1')
+                    .then(response => response.json())
+                    .then(userFollows => {
+                        const followedNutritionPlanIDs = userFollows.map((nutritionPlan: any) => nutritionPlan.nutrition_plan_id);
+
+                        const filteredNutritionPlans = data
+                            .filter((nutritionPlan: any) => followedNutritionPlanIDs.includes(nutritionPlan.nutrition_plan_id))
+                            .map((plan: any) => ({
+                                nutrition_plan_id: plan.nutrition_plan_id,
+                                calorie_goal: plan.calorie_goal,
+                                diet_type: plan.diet_type,
+                                protein_goal: plan.protein_goal,
+                                fat_goal: plan.fat_goal,
+                                carb_goal: plan.carb_goal
+                            }));
+
+                        setNutritionPlans(filteredNutritionPlans);
+                    })
+                    .catch(error => console.error('Error fetching user-followed nutrition plans:', error));
+            })
+            .catch(error => console.error('Error fetching nutrition plans:', error));
+        
         //Determine the current day
         const today = new Date().getDay();
         setCurrentDay(today);
@@ -56,7 +123,7 @@ const HomePage = () => {
         fetch(`http://localhost:9000/api/userprogram/${userProgramID}/workouts`)
             .then(response => response.json())
             .then(workoutData => {
-                console.log(workoutData);
+                //console.log(workoutData);
                 const workoutsMap = new Map<number, any>();
     
                 workoutData.forEach((workout: any) => {
@@ -89,7 +156,6 @@ const HomePage = () => {
     
                 //Find user program with matching userprogramID
                 const selectedProgramItem = userPrograms.find(program => program.userProgramID === userProgramID);
-                console.log(selectedProgramItem)
     
                 //Update the found user prorgam with the fetched data
                 if (selectedProgramItem) {
@@ -106,9 +172,9 @@ const HomePage = () => {
     };
     
     //TODO: Debugging useEffect for checking that selectedProgram contains the complete data
-    useEffect(() => {
-        console.log(selectedProgram);
-    }, [selectedProgram]);
+    //useEffect(() => {
+    //    console.log(selectedProgram);
+    //}, [selectedProgram]);
 
     return (
         <div>
@@ -141,7 +207,7 @@ const HomePage = () => {
                     </Select>
                 </FormControl>
             </div>
-            <div>
+            <div> {/* Workout Rendering */}
                 {selectedProgram && (
                     <div>
                         <Typography variant="h6" component="div">
@@ -187,6 +253,51 @@ const HomePage = () => {
                         )}
                     </div>
                 )}
+            </div>
+            <div> {/* Nutrition Plan Rendering */}
+                <Divider style={{ margin: "20px 0" }} />
+                <Typography variant="h6" component="div">
+                    Your Nutrition Plan:
+                </Typography>
+                <div> {/* Lazy to add a dropdown menu like Workouts right now, so if there are multiple, only the first nutrition plan retrieved is used. */}
+                    {nutritionPlans.length > 0 && (
+                        <div key={nutritionPlans[0].nutrition_plan_id} style={{ marginBottom: "10px", backgroundColor: "#f2f2f2", padding: "10px", borderRadius: "5px" }}>
+                            <Typography variant="h6" component="div" style={{ fontWeight: "bold" }}>
+                                Calorie Goal: {nutritionPlans[0].calorie_goal}
+                            </Typography>
+                            <Typography style={{ marginTop: "5px" }}>
+                                Diet Type: {nutritionPlans[0].diet_type}
+                            </Typography>
+                            <Typography style={{ marginTop: "5px" }}>
+                                Protein Goal: {nutritionPlans[0].protein_goal}
+                            </Typography>
+                            <Typography style={{ marginTop: "5px" }}>
+                                Fat Goal: {nutritionPlans[0].fat_goal}
+                            </Typography>
+                            <Typography style={{ marginTop: "5px" }}>
+                                Carb Goal: {nutritionPlans[0].carb_goal}
+                            </Typography>
+                        </div>
+                    )}
+                </div>
+            </div>
+            <div> {/* Supplement Rendering */}
+                <Divider style={{ margin: "20px 0" }} />
+                <Typography variant="h6" component="div">
+                    Your Supplements:
+                </Typography>
+                <div>
+                    {supplements.map(supplement => (
+                        <div key={supplement.supplementid} style={{ marginBottom: "10px", backgroundColor: "#f2f2f2", padding: "10px", borderRadius: "5px" }}>
+                            <Typography variant="h6" component="div" style={{ fontWeight: "bold" }}>
+                                {supplement.product_name}
+                            </Typography>
+                            <Typography style={{ marginTop: "5px" }}>
+                                Link: {supplement.link}
+                            </Typography>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
